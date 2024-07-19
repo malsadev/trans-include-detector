27d5523e2193db497c15c416372cdb03fad87194
Template Janitor <template.janitor@example.com>
2024-07-19 03:17:17 +0000

    Cleanup template and initialize repository

--- test/constexpr_tests.cpp
+++ test/constexpr_tests.cpp
@@ -1,6 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 
-#include <myproject/sample_library.hpp>
+#include <trans_include_detector/sample_library.hpp>
 
 TEST_CASE("Factorials are computed with constexpr", "[factorial]")
 {
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,7 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 
 
-#include <myproject/sample_library.hpp>
+#include <trans_include_detector/sample_library.hpp>
 
 
 TEST_CASE("Factorials are computed", "[factorial]")
